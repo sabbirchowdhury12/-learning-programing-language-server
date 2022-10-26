52a080e5dcd8e5dc71d341216c2b1e5ec52d4ab9
sabbir <sabbir@gmail.com>
2022-10-26 14:33:45 +0600

    get all course data

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const allCourses = require('./data/data.json');
 
 
 app.get('/', (req, res) => {
     res.send('hellow world');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(allCourses);
+});
+
+
 
 app.listen(port, () => {
     console.log('my-news server is running');
